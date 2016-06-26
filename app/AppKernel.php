7a5624ce44d2914002a65dae9b0565bba5878fdc
kseta <keigo.seta@gmail.com>
2016-06-26 11:17:58 +0900

    [app] AppKernel に HautelookAliceBundle を追加

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -25,10 +25,14 @@ class AppKernel extends Kernel
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
+            // Symfony Standard Bundles
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            // OSS Bundles
+            $bundles[] = new Hautelook\AliceBundle\HautelookAliceBundle();
+            // Application Bundles
         }
 
         return $bundles;
