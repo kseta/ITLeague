aa7da9b678051918431e4fa5ee0aa7924862f475
kseta <keigo.seta@gmail.com>
2016-06-26 23:12:50 +0900

    [Entity][Repository] SeasonEntity, SeasonRepository を追加

---
+++ src/AppBundle/Entity/Season.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Season
+ *
+ * @ORM\Table(name="season")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\SeasonRepository")
+ */
+class Season
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, unique=true)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="year", type="string", length=255)
+     */
+    private $year;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="league_id", type="integer")
+     */
+    private $leagueId;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Season
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     * @return Season
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string 
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set year
+     *
+     * @param string $year
+     * @return Season
+     */
+    public function setYear($year)
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+
+    /**
+     * Get year
+     *
+     * @return string 
+     */
+    public function getYear()
+    {
+        return $this->year;
+    }
+
+    /**
+     * Set leagueId
+     *
+     * @param integer $leagueId
+     * @return Season
+     */
+    public function setLeagueId($leagueId)
+    {
+        $this->leagueId = $leagueId;
+
+        return $this;
+    }
+
+    /**
+     * Get leagueId
+     *
+     * @return integer 
+     */
+    public function getLeagueId()
+    {
+        return $this->leagueId;
+    }
+}
