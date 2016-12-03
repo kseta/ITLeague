502dd3b4df8abebe0cf495face9cd8cfdceda76d
kseta <keigo.seta@gmail.com>
2016-12-03 15:02:02 +0900

    [Criteria] Criteria を追加

---
+++ src/AppBundle/Entity/Criteria/Criteria.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace AppBundle\Entity\Criteria;
+
+use Doctrine\Common\Collections\Criteria as DoctrineCriteria;
+
+class Criteria extends DoctrineCriteria implements CriteriaInterface
+{
+}
