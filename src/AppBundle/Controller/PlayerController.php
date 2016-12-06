cb1562ed28a4e8ef0e1ca0793a05c7e87218681e
kseta <keigo.seta@gmail.com>
2016-12-06 10:38:36 +0900

    [Controller] Player を取得する REST API を追加

---
+++ src/AppBundle/Controller/PlayerController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Routing\ClassResourceInterface;
+
+class PlayerController extends FOSRestController implements ClassResourceInterface
+{
+    public function cgetAction()
+    {
+        $criteria = $this->container->get('domain.criteria_builder.search_player')->build();
+        $players  = $this->container->get('domain.usecase.query.search_player')->run($criteria);
+
+        return $players;
+    }
+
+    public function getAction($id)
+    {
+        $criteriaBuilder = $this->container->get('domain.criteria_builder.search_player');
+        $criteriaBuilder->setPlayerIds([$id]);
+        $criteria = $criteriaBuilder->build();
+        $players  = $this->container->get('domain.usecase.query.search_player')->run($criteria);
+
+        return $players;
+    }
+}
